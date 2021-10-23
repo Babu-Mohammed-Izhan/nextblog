253699893fcbc9793c6765c7b9804569ffbde252
Izhan <babuizhan1234@gmail.com>
2021-10-23 19:34:48 +0530

    added layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Layout.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div></div>
+      <Head>
+        <title>Web Dev Blog</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+      <h1> Welcome to Next</h1>
     </div>
   );
 }
